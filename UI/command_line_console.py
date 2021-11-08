3918a2ea3e507453e7645be793c470eeb8ba768a
Furca-Diana <91545236+Furca-Diana@users.noreply.github.com>
2021-11-08 08:58:42 +0200

    Lab.6

--- UI/command_line_console.py
+++ UI/command_line_console.py
@@ -2,41 +2,57 @@ from Logic.CRUD import adaugarezervare, stergerezervare
 from UI.console import showAll
 
 
-def oneLineAddShowallDelete(elem, lista):
-    elem = elem.split(";")
-    i = 0
-    while i < len(elem):
-        if elem[i] == "Adauga":
-            lista = adaugarezervare(elem[i+1], elem[i+2], elem[i+3], elem[i+4], elem[i+5], lista)
-            i = i + 5
-        elif elem[i] == "Afiseaza toate rezervarile":
-            showAll(lista)
-        elif elem[i] == "Sterge":
-            lista = stergerezervare(elem[i+1], lista)
-        i = i + 1
-    return lista
+def oneLine(lista):
+    while True:
+        lineCom = input('Dati comanda(comenzile separate prin ";" , elementele prin ",": )')
+        lineCom = lineCom.split(';')
+        for l in lineCom:
+            l = l.split(',')
+            if l[0] == 'add':
+                if len(l) == 6:
+                    try:
+                        id = l[1]
+                        nume = l[2]
+                        clasa = l[3]
+                        pret = float(l[4])
+                        checkin = l[5]
+                        lista = adaugarezervare(id, nume, clasa, pret, checkin, lista)
+                    except ValueError as ve:
+                        print(f'Eroare:{ve}')
+                else:
+                    print('Eroare, numar invalid de parametrii')
+            elif l[0] == "showall":
+                showAll(lista)
+            elif l[0] == "delete":
+                if len(l) == 2:
+                    try:
+                        id = l[1]
+                        lista = stergerezervare(id, lista)
+                    except ValueError as ve:
+                        print(f'Eroare:{ve}')
+                else:
+                    print('Eroare, numar invalid de parametrii')
+            else:
+                print('Comanda gresita, reincercati')
 
 
-def uiOneLine(lista):
-    elem = input("Dati comanda cu parametrii corespunzatori: la afisare trebuie id, nume, clasa, pret, checkin/"
-                 "la stergere trebuie id: ")
-    return oneLineAddShowallDelete(elem, lista)
-
-
-def printMeniu():
-    print("1. Adauga/ Sterge/ Afiseaza toate rezervarile")
+def printMeniu2():
+    print("1. Adauga o rezervare/ Sterge o rezervare/ Afiseaza toate rezervarile")
     print("x. Iesire")
-    print("help. Printare meniu")
+    print("h. Help.")
 
 
 def runMenu2(lista):
     while True:
-        printMeniu()
+        printMeniu2()
         optiune = input("Alegeti optiunea: ")
         if optiune == "help":
-            printMeniu()
+            print("Daca vreti sa adaugati o rezervare, scrieti comanda add, urmata de id, nume, clasa, pret, checkin")
+            print("Daca vreti sa stergeti o rezervare, scrieti comanda delete, urmata de id")
+            print("Daca vreti sa afisati toata lista, scrieti showall")
+
         elif optiune == "1":
-            lista = uiOneLine(lista)
+            lista = oneLine(lista)
         elif optiune == "x":
             print("Programul a fost incheiat")
             break
--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from UI.command_line_console import runMenu2
 from Logic.CRUD import adaugarezervare
 
 
-def optiuni():
-    print("1. Alege meniul standard.")
-    print("2. Alege meniul in care poti apela mai multe functii in acelasi timp!")
+def alegmeniu():
+    print("1. Alege meniul standard- Interfata 1.")
+    print("2. Alege meniul in care poti apela mai multe functii in acelasi timp!- Interfata 2")
 
 
 def main():
@@ -17,23 +17,21 @@ def main():
     lista = adaugarezervare("3", "Liviu", "economy", 250, "nu", lista)
     lista = adaugarezervare("4", "Denisa", "business", 550, "nu", lista)
     lista = adaugarezervare("5", "Ada", "economy plus", 800, "da", lista)
-    optiuni()
-    optiune = input("Alege main-ul pe care vrei sa il rulezi: ")
 
     while True:
+        alegmeniu()
+        optiune = input("Alege interfata: ")
         if optiune == "1":
             runMenu(lista)
         elif optiune == "2":
             runMenu2(lista)
-        else:
-            print("Meniul selectat nu exista! Reincercati.")
-        meniuNou = input("Vreti sa schimbati meniul? da/nu:")
-        if meniuNou.lower() == "da":
-            optiuni()
-            optiune = input("Alege main-ul pe care vrei sa il rulezi")
-        elif meniuNou.lower() == "nu":
+        elif optiune == "help":
+            print("Alege un meniu dintre Interfata 1 si Interfata 2:")
+        elif optiune == "x":
             break
         else:
-            print("Meniul selectat nu exista! Reincercati.")
+            print("Optiune greasita! Reincercati!")
 
-main()
+
+if __name__ == '__main__':
+    main()
